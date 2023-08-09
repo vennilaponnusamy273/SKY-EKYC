84669dbaa6fe368ac76abaa28499d3f887cfb93d
vennilaponnusamy273 <vennila@codifi.in>
2023-08-09 11:18:21 +0530

    Update Email Host

--- src/main/java/in/codifi/api/utilities/CommonMethods.java
+++ src/main/java/in/codifi/api/utilities/CommonMethods.java
@@ -497,7 +497,9 @@ public class CommonMethods {
 			emailLogEntity.setEmailId(mailId);
 			emailLogEntity.setLogMethod(logMethod);
 			emailLogEntity.setReqLogSub(ReqSub);
-			emailLogEntity.setReqLog(message);
+			// Encode the HTML content using Base64 encoding
+	        String encodedMessage = Base64.getEncoder().encodeToString(message.getBytes());
+	        emailLogEntity.setReqLog(encodedMessage);
 			emailLogEntity.setResponseLog(emailResponse);
 			emailLogRepository.save(emailLogEntity);
 		} catch (Exception e) {
